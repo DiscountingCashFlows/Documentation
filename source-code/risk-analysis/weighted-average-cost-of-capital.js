e00c129a3eafef93ec01fde9c1d1ef58c5dd29c4
Imbrea Bogdan-Andrei <46221053+bogdanimbrea@users.noreply.github.com>
2022-09-12 23:17:34 +0300

    Update weighted-average-cost-of-capital.js

--- source-code/risk-analysis/weighted-average-cost-of-capital.js
+++ source-code/risk-analysis/weighted-average-cost-of-capital.js
@@ -1,8 +1,8 @@
 // +------------------------------------------------------------+
-// | Model: WACC (Weighted Average Cost of Capital)				|
-// | Copyright: https://discountingcashflows.com, 2022			|
+//   Model: WACC (Weighted Average Cost of Capital)				
+//   Copyright: https://discountingcashflows.com, 2022			
 // +------------------------------------------------------------+
-var INPUT = Input({_MARKET_RETURN: 7,
+var INPUT = Input({_MARKET_PREMIUM: 5.5,
   				   _RISK_FREE_RATE: '',
                    BETA: ''}); 
 
@@ -11,30 +11,38 @@ $.when(
   get_balance_sheet_statement_quarterly(),
   get_profile(),
   get_treasury()).done(
-  function(income, balance_quarter, profile, treasury){
-    balance_quarter = balance_quarter[0];
-    income = income[0];
+  function(_income_ltm, _balance_quarterly, _profile, _treasury){
+    var context = [];
+    // Create deep copies of reports. This section is needed for watchlist compatibility.
+    var income_ltm = JSON.parse(JSON.stringify(_income_ltm));
+    var balance_last_quarter = JSON.parse(JSON.stringify(_balance_quarterly));
+    var profile = JSON.parse(JSON.stringify(_profile));
+    var treasury = JSON.parse(JSON.stringify(_treasury));
+    
+    balance_last_quarter = balance_last_quarter[0][0];
+    income_ltm = income_ltm[0];
     profile = profile[0][0];
+    treasury = treasury[0][0];
 
 	setInputDefault('BETA', profile['beta']);
-	setInputDefault('_RISK_FREE_RATE', treasury[0][0]['year10']);
+	setInputDefault('_RISK_FREE_RATE', treasury['year10']);
     
     
     // Cost of Debt
-    var costOfDebt = income['interestExpense'] / balance_quarter[0]['totalDebt']; // Total Debt = Short Term Debt + Long Term Debt
+    var costOfDebt = income_ltm['interestExpense'] / balance_last_quarter['totalDebt']; // Total Debt = Short Term Debt + Long Term Debt
 	
     // Tax Rate
-    var taxRate = income['incomeTaxExpense'] / income['incomeBeforeTax'];
+    var taxRate = income_ltm['incomeTaxExpense'] / income_ltm['incomeBeforeTax'];
     if(taxRate < 0)
     {
 		taxRate = 0;
     }
     
     // Cost of Equity
-    var costOfEquity = INPUT._RISK_FREE_RATE + INPUT.BETA*(INPUT._MARKET_RETURN - INPUT._RISK_FREE_RATE);
+    var costOfEquity = INPUT._RISK_FREE_RATE + INPUT.BETA*INPUT._MARKET_PREMIUM;
     
     // Weights
-    var totalDebt = balance_quarter[0]['shortTermDebt'] + balance_quarter[0]['longTermDebt'];
+    var totalDebt = balance_last_quarter['shortTermDebt'] + balance_last_quarter['longTermDebt'];
     var marketCap = profile['mktCap'];
     
     var debtWeight = totalDebt / (marketCap + totalDebt);
@@ -50,35 +58,46 @@ $.when(
 	print(wacc, "WACC", '%');
     print(equityWeight, 'Equity Weight', '%');
     print(costOfEquity, "Cost of Equity", '%');
-    print(INPUT._MARKET_RETURN - INPUT._RISK_FREE_RATE, 'Market Premium', '%');
     print(debtWeight, 'Debt Weight', '%');
     print(costOfDebt, "Cost of Debt", '%');
     print(taxRate, "Tax Rate", '%');
     
-    var context = [];
-	
+	// Check if the currency is being converted 
+    // The profile can have a different currency from the reports.
 	var currency = '';
-	if('convertedCurrency' in balance_quarter[0]){
-		currency = balance_quarter[0]['convertedCurrency'];
+    var currencyProfile = '';
+    if('convertedCurrency' in profile){
+		currencyProfile = profile['convertedCurrency'];
 	}else{
-		currency = balance_quarter[0]['reportedCurrency'];
+		currencyProfile = profile['currency'];
 	}
+	if('convertedCurrency' in balance_last_quarter){
+		currency = balance_last_quarter['convertedCurrency'];
+	}else{
+		currency = balance_last_quarter['reportedCurrency'];
+	}
+    
+    // If the profile and reports currencies differ from each other, the user needs to select a currency from the top right menu to get the values in one currency.
+    if( currencyProfile != currency ){
+    	warning("The market price currency(" + currencyProfile + ") and the financial report's currency(" + currency + ") do not match! Please select a curreny from the top right menu.");
+      	return;
+    }
 
     contextItem = {name:'WACC Calculation (In Millions of ' + currency + ' except for rates)', display:'table', 
                    rows:['WACC%','Interest Expense', 'Short Term Debt', 'Long Term Debt', 'Total Debt', 
                          'Cost of Debt%', 'Cost of Equity%', 'Market Cap', 'Debt Weight%', 'Equity Weight%', 'Income Tax Expense', 'Income Before Tax', 'Tax Rate%'], 
                    columns:['Values'], data:[[(100 * wacc).toFixed(2) + '%'], 
-                                             [income['interestExpense']/1000000], 
-                                             [balance_quarter[0]['shortTermDebt']/1000000], 
-                                             [balance_quarter[0]['longTermDebt']/1000000], 
-                                             [balance_quarter[0]['totalDebt']/1000000], 
+                                             [toM(income_ltm['interestExpense'])], 
+                                             [toM(balance_last_quarter['shortTermDebt'])], 
+                                             [toM(balance_last_quarter['longTermDebt'])], 
+                                             [toM(balance_last_quarter['totalDebt'])], 
                                              [(100 * costOfDebt).toFixed(2) + '%'],
                                              [(100 * costOfEquity).toFixed(2) + '%'],
-                                             [(profile['mktCap']/1000000).toFixed(2)], 
+                                             [(toM(profile['mktCap'])).toFixed(2)], 
                                              [(100 * debtWeight).toFixed(2) + '%'],
                                              [(100 * equityWeight).toFixed(2) + '%'],
-                                             [income['incomeTaxExpense']/1000000], 
-                                             [income['incomeBeforeTax']/1000000],
+                                             [toM(income_ltm['incomeTaxExpense'])], 
+                                             [toM(income_ltm['incomeBeforeTax'])],
                                              [(100 * taxRate).toFixed(2) + '%']
                                             ]};
     context.push(contextItem);
@@ -86,29 +105,7 @@ $.when(
 });
 
 var DESCRIPTION = Description(`
-								<h5>WACC (Weighted Average Cost of Capital)</h5> 
-								WACC is a financial metric that helps in calculating a firm’s cost of financing by combining the cost of debt and cost of equity structure together.
-								<p>Reference: <a href='https://www.educba.com/wacc-formula/' target='_blank'>www.educba.com</a></p>
-                                <small>*Please note that WACC values are not correctly computed for companies which report in other currencies except USD.</small>
-								`, `
-                                Formula used:
-                                <div class="d-block text-center my-2">
-                                  \\( WACC =  \\) \\( [(EquityWeight * CostOfEquity) + (DebtWeight * CostOfDebt)] *  \\) \\( (1 - TaxRate) \\)
-                                </div>
-                                <ul>
-                                <li>Equity Weight Formula:</li>
-                                $$ EquityWeight = {MarketCap \\over (MarketCap + TotalDebt)} $$
-								<li>Cost of Equity Formula(Equivalent to CAPM <a href='https://www.investopedia.com/terms/c/capm.asp' target='_blank'>investopedia.com</a>):</li>
-                                $$ ER\_i = R\_f + \\beta * (ER\_m - R\_f) $$
-                                <ul>
-                                <li>\\( ER\_i \\) : Expected return of the stock</li>
-								<li>\\( R\_f \\) : The Risk Free Rate is the yield of the 10 Year Treasury Note today</li>
-                                <li>\\( \\beta \\) : A measure of how risky the stock is compared to the market. Learn more: <a href="https://www.investopedia.com/terms/b/beta.asp">investopedia.com</a></li>
-                                <li>\\( ER\_m \\) : Expected return of the broad market. In general markets yield on average 7% annually</li>
-                                </ul>
-                                <li>Debt Weight Formula:</li>
-                                $$ DebtWeight = {TotalDebt \\over (MarketCap + TotalDebt)} $$
-                                <li>Cost of Debt Formula:</li>
-                                $$ CostOfDebt = {InterestExpense \\over TotalDebt} $$
-                                </ul>
-`);
+								<h5>Weighted Average Cost of Capital (WACC)</h5> 
+								<p>WACC is a financial metric that helps in calculating a firm’s cost of financing by combining the cost of debt and cost of equity structure together.</p>
+								<p class='text-center'>Read more: <a href='https://github.com/DiscountingCashFlows/Documentation/blob/main/models-documentation/discounted-free-cash-flow.md#discount-rate-wacc---weighted-average-cost-of-capital' target='_blank'><i class="fab fa-github"></i> GitHub Documentation</a></p>
+                                `);
