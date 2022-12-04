62722b3151743887a146a132fe8be77c1e9bb8f9
Imbrea Bogdan-Andrei <46221053+bogdanimbrea@users.noreply.github.com>
2022-12-04 15:50:51 +0200

    Update capital-asset-pricing-model.js

--- source-code/risk-analysis/capital-asset-pricing-model.js
+++ source-code/risk-analysis/capital-asset-pricing-model.js
@@ -1,6 +1,6 @@
 // +------------------------------------------------------------+
 //   Model: Capital Asset Pricing Model (CAPM)					
-//   Copyright: https://discountingcashflows.com, 2022			
+//   © Copyright: https://discountingcashflows.com	
 // +------------------------------------------------------------+
 var INPUT = Input({_MARKET_PREMIUM: 5.5,
   				   _RISK_FREE_RATE: '',
@@ -11,11 +11,8 @@ $.when(
   get_treasury()).done(
   function(_profile, _treasury){
     // Create deep copies of reports. This section is needed for watchlist compatibility.
-    var profile = JSON.parse(JSON.stringify(_profile));
-    var treasury = JSON.parse(JSON.stringify(_treasury));
-    
-    profile = profile[0][0];
-    treasury = treasury[0][0];
+    var profile = deepCopy(_profile);
+    var treasury = deepCopy(_treasury);
 
     setInputDefault('BETA', profile['beta']);
 	setInputDefault('_RISK_FREE_RATE', treasury['year10']);
@@ -31,8 +28,8 @@ $.when(
     print(expectedReturn, "Cost of Equity", '%');
 });
 
-var DESCRIPTION = Description(`
-								<h5>Capital Asset Pricing Model (CAPM)</h5> 
-								The Capital Asset Pricing Model describes the relationship between risk and expected return for assets, particularly stocks. Its formula is used to calculate the cost of equity, or the rate of return a company is expected to pay to equity investors.
-								<p class='text-center'>Read more: <a href='https://github.com/DiscountingCashFlows/Documentation/blob/main/models-documentation/dividend-discount-models.md#discount-rate-cost-of-equity' target='_blank'><i class="fab fa-github"></i> GitHub Documentation</a></p>
-                                `);
+Description(`
+	<h5>Capital Asset Pricing Model (CAPM)</h5> 
+	The Capital Asset Pricing Model describes the relationship between risk and expected return for assets, particularly stocks. Its formula is used to calculate the cost of equity, or the rate of return a company is expected to pay to equity investors.
+	<p class='text-center'>Read more: <a href='https://github.com/DiscountingCashFlows/Documentation/blob/main/models-documentation/dividend-discount-models.md#discount-rate-cost-of-equity' target='_blank'><i class="fab fa-github"></i> GitHub Documentation</a></p>
+`);
